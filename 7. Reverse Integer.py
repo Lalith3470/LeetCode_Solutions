7b9e12faad1227f2ab850fbe0b76e7d9a1328121
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-11-25 19:27:43 +0530

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,11 +1,9 @@
 def reverse(self, x: int) -> int:
-        a = str(abs(x))
-        a = a.strip()
-        a = a[::-1]
-        b = int(a)
-        if b > 2**31-1 or b<-2**31:
+        a = str(abs(x)).strip()
+        a = int(a[::-1])
+        if a > 2**31-1 or a<-2**31:
             return 0
         elif x < 0:
-            return b*-1
+            return a*-1
         else:
-            return b
+            return a
