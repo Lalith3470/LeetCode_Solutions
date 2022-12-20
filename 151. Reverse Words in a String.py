05091d8265f313e3acf34f12414b069367faba22
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-12-20 20:44:43 +0530

    Update 151. Reverse Words in a String.py

--- 151. Reverse Words in a String.py
+++ 151. Reverse Words in a String.py
@@ -3,9 +3,7 @@ class Solution:
         s=s.split()
         rev_str=''
         for i in reversed(range(len(s))):
-            if i==0:
-                rev_str+=s[i]
-            else:
-                rev_str+=s[i]+' '
+            if i==0:rev_str+=s[i]
+            else:rev_str+=s[i]+' '
                 
         return rev_str
