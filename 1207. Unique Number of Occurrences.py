e8f3cb2c6a5513e07b580f165ab60a55c7e3906c
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-12-20 20:43:01 +0530

    Update 1207. Unique Number of Occurrences.py

--- 1207. Unique Number of Occurrences.py
+++ 1207. Unique Number of Occurrences.py
@@ -2,6 +2,7 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         lst=[]
         count=Counter(arr)
+        
         for i,j in count.items():
             lst.append(j)
             
