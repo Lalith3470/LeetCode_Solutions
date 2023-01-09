2b48144bd69b8a7abdc377aa6236e5087e8b8f3f
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-01-09 23:18:06 +0530

    Create 347. Top K Frequent Elements.py

---
+++ 347. Top K Frequent Elements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq={}
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+        return sorted(freq, key=freq.get, reverse=True)[:k]
