1441130de5c443bac650cf32f7aa310f83aabc0d
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-02-05 21:07:39 +0530

    Create 48. Rotate Image.py

---
+++ 48. Rotate Image.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        lst=[]
+        for i in zip(*matrix):
+            lst.append(i[::-1])
+        matrix[:]=lst
