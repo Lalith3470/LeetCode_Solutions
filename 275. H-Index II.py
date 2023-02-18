e1526bbf22eb0d3b2e8258d06759139a95468a24
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-02-18 21:06:23 +0530

    Create 275. H-Index II.py

---
+++ 275. H-Index II.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations=citations[::-1]
+        for i in range(len(citations)):
+            #print(i, citations[i])
+            if citations[i]<=i:
+                return i
+        return len(citations)     
