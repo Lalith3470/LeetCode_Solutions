5f53bc8acaf541e45b8959f1a21e5c9279be7375
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-04-23 20:54:44 +0530

    Create 62. Unique Paths.cpp

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0; j<n;j++){
+                if(i==0&&j==0){
+                    continue;
+                }
+                else{
+                    int up=0,down=0;
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0)down=dp[i][j-1];
+                    dp[i][j]=up+down;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
