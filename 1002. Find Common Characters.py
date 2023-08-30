d52fff4ccd7a3f8e141ffa6289e34fdd45cc4300
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-08-30 23:56:51 +0530

    Update 1002. Find Common Characters.py

--- 1002. Find Common Characters.py
+++ 1002. Find Common Characters.py
@@ -1,10 +1,9 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
         cmn=Counter(words[0])
-        
+        lst=[]
         for word in range(1,len(words)):
             cmn=cmn & Counter(words[word])
-        lst=[]
         for i,j in cmn.items():
             for res in range(j):
                 lst.append(i)
