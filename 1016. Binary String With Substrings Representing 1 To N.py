fb588ae59fa39f6324d466a58d9c3ca18ecaa7cf
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-09-10 20:28:36 +0530

    Update 1016. Binary String With Substrings Representing 1 To N.py

--- 1016. Binary String With Substrings Representing 1 To N.py
+++ 1016. Binary String With Substrings Representing 1 To N.py
@@ -1,6 +1,6 @@
 class Solution:
     def queryString(self, s: str, n: int) -> bool:
         for i in range(1,n+1):
-            
+            #binary
             if bin(i)[2:] not in s:return False
         return True
